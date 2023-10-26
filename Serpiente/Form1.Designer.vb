62a013cf2d23fb793570493b59df1905f0b06ef2
Adrian Mendoza <catorce_de_enero@hotmail.com>
2023-10-25 20:55:57 -0600

    Add project files.

---
+++ Serpiente/Form1.Designer.vb
@@ -0,0 +1,293 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSnake
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmSnake))
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox7 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox9 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox10 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.tiempo = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(702, 593)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(52, 17)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Arriba"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        Me.RadioButton1.Visible = False
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(668, 616)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(39, 17)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Izq"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        Me.RadioButton2.Visible = False
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(731, 616)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(42, 17)
+        Me.RadioButton3.TabIndex = 2
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "Der"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        Me.RadioButton3.Visible = False
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(702, 639)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(52, 17)
+        Me.RadioButton4.TabIndex = 3
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "Abajo"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        Me.RadioButton4.Visible = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 450
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(68, 604)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(50, 50)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 6
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Image = CType(resources.GetObject("PictureBox5.Image"), System.Drawing.Image)
+        Me.PictureBox5.Location = New System.Drawing.Point(180, 604)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(50, 50)
+        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox5.TabIndex = 7
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = CType(resources.GetObject("PictureBox4.Image"), System.Drawing.Image)
+        Me.PictureBox4.Location = New System.Drawing.Point(124, 604)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(50, 50)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 8
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.Image = CType(resources.GetObject("PictureBox6.Image"), System.Drawing.Image)
+        Me.PictureBox6.Location = New System.Drawing.Point(236, 604)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(50, 50)
+        Me.PictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox6.TabIndex = 12
+        Me.PictureBox6.TabStop = False
+        '
+        'PictureBox7
+        '
+        Me.PictureBox7.Image = CType(resources.GetObject("PictureBox7.Image"), System.Drawing.Image)
+        Me.PictureBox7.Location = New System.Drawing.Point(292, 604)
+        Me.PictureBox7.Name = "PictureBox7"
+        Me.PictureBox7.Size = New System.Drawing.Size(50, 50)
+        Me.PictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox7.TabIndex = 11
+        Me.PictureBox7.TabStop = False
+        '
+        'PictureBox8
+        '
+        Me.PictureBox8.Image = CType(resources.GetObject("PictureBox8.Image"), System.Drawing.Image)
+        Me.PictureBox8.Location = New System.Drawing.Point(345, 604)
+        Me.PictureBox8.Name = "PictureBox8"
+        Me.PictureBox8.Size = New System.Drawing.Size(50, 50)
+        Me.PictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox8.TabIndex = 10
+        Me.PictureBox8.TabStop = False
+        '
+        'PictureBox9
+        '
+        Me.PictureBox9.Image = CType(resources.GetObject("PictureBox9.Image"), System.Drawing.Image)
+        Me.PictureBox9.Location = New System.Drawing.Point(401, 604)
+        Me.PictureBox9.Name = "PictureBox9"
+        Me.PictureBox9.Size = New System.Drawing.Size(39, 43)
+        Me.PictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox9.TabIndex = 9
+        Me.PictureBox9.TabStop = False
+        '
+        'PictureBox10
+        '
+        Me.PictureBox10.Image = CType(resources.GetObject("PictureBox10.Image"), System.Drawing.Image)
+        Me.PictureBox10.Location = New System.Drawing.Point(446, 604)
+        Me.PictureBox10.Name = "PictureBox10"
+        Me.PictureBox10.Size = New System.Drawing.Size(50, 50)
+        Me.PictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox10.TabIndex = 13
+        Me.PictureBox10.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(12, 604)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(50, 50)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 5
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(350, 150)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(50, 50)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label1.Location = New System.Drawing.Point(715, 516)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 14
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(575, 600)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(59, 41)
+        Me.Label2.TabIndex = 15
+        Me.Label2.Text = "40"
+        '
+        'tiempo
+        '
+        Me.tiempo.Enabled = True
+        Me.tiempo.Interval = 1000
+        '
+        'frmSnake
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(792, 666)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox10)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.PictureBox7)
+        Me.Controls.Add(Me.PictureBox8)
+        Me.Controls.Add(Me.PictureBox9)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.RadioButton4)
+        Me.Controls.Add(Me.RadioButton3)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Name = "frmSnake"
+        Me.Text = "v "
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton3 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton4 As System.Windows.Forms.RadioButton
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox5 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox4 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox6 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox7 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox8 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox9 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox10 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents tiempo As System.Windows.Forms.Timer
+
+End Class
---
+++ Serpiente/Form1.vb
@@ -0,0 +1,296 @@
+﻿Public Class frmSnake
+    Dim x = 0
+    Dim y = 0
+    Dim x1 = 0
+    Dim y1 = 0
+    Dim x2 = 0
+    Dim y2 = 0
+    Dim x3 = 0
+    Dim y3 = 0
+    Dim x4 = 0
+    Dim y4 = 0
+    Dim x5 = 0
+    Dim y5 = 0
+    Dim x6 = 0
+    Dim y6 = 0
+    Dim x7 = 0
+    Dim y7 = 0
+    Dim x8 = 0
+    Dim y8 = 0
+    Dim x9 = 0
+    Dim y9 = 0
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        If (RadioButton1.Checked) Then
+            PictureBox1.Top -= 50
+        End If
+        If (RadioButton4.Checked) Then
+            PictureBox1.Top += 50
+        End If
+        If (RadioButton2.Checked) Then
+            PictureBox1.Left -= 50
+        End If
+        If (RadioButton3.Checked) Then
+            PictureBox1.Left += 50
+        End If
+
+        If (a = 9) Then
+            Timer1.Stop()
+            MsgBox("Ganaste")
+            End
+        End If
+        'p1,p2,p3,a
+        If ((a = 1) And (PictureBox1.Location.X = PictureBox2.Location.X) And (PictureBox1.Location.Y = PictureBox2.Location.Y)) Then
+            a = 2
+            PictureBox3.Top = ejeY()
+            PictureBox3.Left = ejeX()
+        End If
+
+
+        If (a >= 2) Then
+            PictureBox2.Top = y
+            PictureBox2.Left = x
+            x = PictureBox1.Location.X
+            y = PictureBox1.Location.Y
+        End If
+        'p1,p3,p4,p2,a
+        'sigima(PictureBox1,PictureBox3 )
+        If ((a >= 2) And (PictureBox1.Location.X = PictureBox3.Location.X) And (PictureBox1.Location.Y = PictureBox3.Location.Y)) Then
+            a = 3
+            PictureBox4.Top = ejeY()
+            PictureBox4.Left = ejeX()
+            x1 = PictureBox2.Left
+            y1 = PictureBox2.Top
+        End If
+
+        If (a >= 3) Then
+            PictureBox3.Top = y1
+            PictureBox3.Left = x1
+            x1 = PictureBox2.Location.X
+            y1 = PictureBox2.Location.Y
+        End If
+        If ((a >= 3) And (PictureBox1.Location.X = PictureBox4.Location.X) And (PictureBox1.Location.Y = PictureBox4.Location.Y)) Then
+            a = 4
+            PictureBox5.Top = ejeY()
+            PictureBox5.Left = ejeX()
+            x2 = PictureBox3.Left
+            y2 = PictureBox3.Top
+            Timer1.Interval = 300
+        End If
+
+
+        If (a >= 4) Then
+            PictureBox4.Top = y2
+            PictureBox4.Left = x2
+            x2 = PictureBox3.Location.X
+            y2 = PictureBox3.Location.Y
+        End If
+        If ((a >= 4) And (PictureBox1.Location.X = PictureBox5.Location.X) And (PictureBox1.Location.Y = PictureBox5.Location.Y)) Then
+            a = 5
+            PictureBox6.Top = ejeY()
+            PictureBox6.Left = ejeX()
+            x3 = PictureBox4.Left
+            y3 = PictureBox4.Top
+        End If
+
+
+        If (a >= 5) Then
+            PictureBox5.Top = y3
+            PictureBox5.Left = x3
+            x3 = PictureBox4.Location.X
+            y3 = PictureBox4.Location.Y            
+            Label1.Text = a
+        End If
+        If ((a >= 5) And (PictureBox1.Location.X = PictureBox6.Location.X) And (PictureBox1.Location.Y = PictureBox6.Location.Y)) Then
+            a = 6
+            PictureBox7.Top = ejeY()
+            PictureBox7.Left = ejeX()
+            x4 = PictureBox5.Left
+            y4 = PictureBox5.Top
+            'MsgBox(y4 & " " & x4)
+        End If
+
+
+        If (a >= 6) Then
+            PictureBox6.Top = y4
+            PictureBox6.Left = x4
+            'MsgBox(y4 & " " & x4)
+            x4 = PictureBox5.Location.X
+            y4 = PictureBox5.Location.Y
+        End If
+
+        If ((a >= 6) And (PictureBox1.Location.X = PictureBox7.Location.X) And (PictureBox1.Location.Y = PictureBox7.Location.Y)) Then
+            a = 7
+            PictureBox8.Top = ejeY()
+            PictureBox8.Left = ejeX()
+            x5 = PictureBox6.Left
+            y5 = PictureBox6.Top
+        End If
+
+        If (a >= 7) Then
+            PictureBox7.Top = y5
+            PictureBox7.Left = x5
+            x5 = PictureBox6.Location.X
+            y5 = PictureBox6.Location.Y
+        End If
+        If ((a >= 7) And (PictureBox1.Location.X = PictureBox8.Location.X) And (PictureBox1.Location.Y = PictureBox8.Location.Y)) Then
+            a = 8
+            PictureBox9.Top = ejeY()
+            PictureBox9.Left = ejeX()
+            x6 = PictureBox7.Left
+            y6 = PictureBox7.Top
+        End If
+
+
+        If (a >= 8) Then
+            PictureBox8.Top = y6
+            PictureBox8.Left = x6
+            x6 = PictureBox7.Location.X
+            y6 = PictureBox7.Location.Y
+        End If
+        If ((a >= 8) And (PictureBox1.Location.X = PictureBox9.Location.X) And (PictureBox1.Location.Y = PictureBox9.Location.Y)) Then
+            a = 9
+            PictureBox9.Top = ejeY()
+            PictureBox9.Left = ejeX()
+            x7 = PictureBox8.Left
+            y7 = PictureBox8.Top
+        End If
+    End Sub
+    Dim a As Integer
+    Dim Vector(0 To 9, 0 To 1) As Integer
+
+    Private Sub Form1_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyDown
+        'MsgBox(e.KeyCode)
+        If e.KeyCode = 39 Then
+            RadioButton3.Select()
+        End If
+        If e.KeyCode = 37 Then
+            RadioButton2.Select()
+        End If
+        If e.KeyCode = 38 Then
+            RadioButton1.Select()
+        End If
+        If e.KeyCode = 40 Then
+            RadioButton4.Select()
+        End If
+
+    End Sub
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        a = 1
+        PictureBox2.Top = ejeY()
+        PictureBox2.Left = ejeX()
+        x = PictureBox2.Left
+        y = PictureBox2.Top
+        Label2.Text = ti
+    End Sub
+
+
+    Dim Na As Double
+    Dim Na1 As Double
+    Private Function ejeX() As Integer
+        'Na = Date.Now.Millisecond
+        Na = CLng((0 - 1000) * Rnd() + 1000)
+
+        If (Na >= 0 And Na <= 62.5) Then
+            Return 0
+        End If
+        If (Na >= 62.5 And Na <= 125) Then
+            Return 50
+        End If
+        If (Na >= 125 And Na <= 187.5) Then
+            Return 100
+        End If
+        If (Na >= 187.5 And Na <= 250) Then
+            Return 150
+        End If
+        If (Na >= 250 And Na <= 312.5) Then
+            Return 200
+        End If
+        If (Na >= 312.5 And Na <= 375) Then
+            Return 250
+        End If
+        If (Na >= 375 And Na <= 437.5) Then
+            Return 300
+        End If
+        If (Na >= 437.5 And Na <= 500) Then
+            Return 350
+        End If
+        If (Na >= 500 And Na <= 562.5) Then
+            Return 400
+        End If
+        If (Na >= 562.5 And Na <= 625) Then
+            Return 450
+        End If
+        If (Na >= 625 And Na <= 687.5) Then
+            Return 500
+        End If
+        If (Na >= 687.5 And Na <= 750) Then
+            Return 550
+        End If
+        If (Na >= 750 And Na <= 812.5) Then
+            Return 600
+        End If
+        If (Na >= 812.5 And Na <= 875) Then
+            Return 650
+        End If
+        If (Na >= 875 And Na <= 937.5) Then
+            Return 700
+        End If
+        If (Na >= 937.5 And Na <= 1000) Then
+            Return 750
+        Else
+            Return 400
+        End If
+
+    End Function
+
+    Private Function ejeY() As Integer
+        Na1 = Date.Now.Millisecond
+        If (Na1 >= 0 And Na1 <= 83.3) Then
+            Return 0
+        End If
+        If (Na1 >= 83.3 And Na1 <= 249.9) Then
+            Return 50
+        End If
+        If (Na1 >= 249.9 And Na1 <= 333.2) Then
+            Return 100
+        End If
+        If (Na1 >= 333.2 And Na1 <= 416.5) Then
+            Return 150
+        End If
+        If (Na1 >= 416.5 And Na1 <= 499.8) Then
+            Return 200
+        End If
+        If (Na1 >= 499.8 And Na1 <= 583.1) Then
+            Return 250
+        End If
+        If (Na1 >= 583.1 And Na1 <= 666.4) Then
+            Return 300
+        End If
+        If (Na1 >= 666.4 And Na1 <= 749.7) Then
+            Return 350
+        End If
+        If (Na1 >= 749.7 And Na1 <= 833) Then
+            Return 400
+        End If
+        If (Na1 >= 833 And Na1 <= 916.3) Then
+            Return 450
+        End If
+        If (Na1 >= 916.3 And Na1 <= 999.6) Then
+            Return 500
+        Else
+            Return 550
+        End If
+    End Function
+    Dim ti = 40
+    Private Sub tiempo_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tiempo.Tick
+        ti -= 1
+        Label2.Text = ti
+        If (ti = 0) Then
+            tiempo.Stop()
+            Timer1.Stop()
+            MsgBox("Perdiste")
+            Application.Restart()
+        End If
+
+    End Sub
+End Class
